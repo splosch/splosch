a1ae769654b25646ad003c4787f3bface937ce0c
splosch <winkler-mario@web.de>
2018-02-17 22:00:35 +0100

    Add basic QRCode Scanner component (not configured yet)

--- src/Scanner.vue
+++ src/Scanner.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="scanner">
     <h1>Scan Me!</h1>
+    <qrcode-reader></qrcode-reader>
     <app-footer></app-footer>
   </div>
 </template>
 
 <script>
+  import { QrcodeReader } from 'vue-qrcode-reader';
   import AppFooter from './components/Footer';
 
   export default {
     name: 'scanner',
     components: {
       AppFooter,
+      QrcodeReader,
     },
   };
 </script>
