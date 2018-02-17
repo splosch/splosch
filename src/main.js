38875996dc801b61eb94ac04b78b69efb09c545c
splosch <winkler-mario@web.de>
2018-02-17 21:52:41 +0100

    Add basic Routing to access Scanner on seperate route than current App

--- src/main.js
+++ src/main.js
@@ -2,10 +2,32 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
+import Scanner from './Scanner';
+
+const NotFound = { template: '<p>Page not found :(</p>' };
+const Home = App;
+const QRScanner = Scanner;
+
+const routes = {
+  '/': Home,
+  '/cube': Home,
+  '/scan': QRScanner,
+};
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   template: '<App/>',
+  data: {
+    currentRoute: window.location.pathname,
+  },
   components: { App },
+  computed: {
+    ViewComponent() {
+      return routes[this.currentRoute] || NotFound;
+    },
+  },
+  render(h) {
+    return h(this.ViewComponent);
+  },
 });
